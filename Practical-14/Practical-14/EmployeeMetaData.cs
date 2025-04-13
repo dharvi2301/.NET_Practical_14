0a1ae5640ac8af8886f14eaca232d27b4bf51c0a
dharvi_kaneriya <dharvikaneriya@gmail.com>
2025-04-13 19:34:31 +0530

    feature: Implemented the Practical 14.

---
+++ Practical-14/Practical-14/EmployeeMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practical_14
+{
+    public class EmployeeMetadata
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required.")]
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
